6cede9c3a06e4c32746f182a4a9a44a90d76343e
IgorSantanaN <124214646+steve-593@users.noreply.github.com>
2023-06-23 16:20:16 -0300

    funções

--- telegram/javascript/funções/ex02/script.js
+++ telegram/javascript/funções/ex02/script.js
@@ -1,9 +1,10 @@
+//Minha versão
+
 let des = (v, d, cb) => {
-    let vel = v
     do {
-        cb(vel)
-        vel -= d
-    } while(vel > 0) 
+        cb(v)
+        v -= d
+    } while(v > 0) 
     cb(0)
     alert('A nave está parada, as comportas podem ser abertas!')
 }
@@ -13,3 +14,22 @@ let callback = (num) => {
 }
 
 des(150, 20, callback)
+
+//versão do curso
+
+function slowDown(velocity, printer) {
+    let deceleration = 20
+
+    while(velocity > 0) {
+        printer(velocity)
+        velocity -= deceleration
+    }
+
+    alert('Nave parada. As comportas podem ser abertas.')
+}
+
+let spaceshipVelocity = 150
+
+slowDown(spaceshipVelocity, function(velocity) {
+    console.log('Velocidade atual: ' + velocity)
+})
