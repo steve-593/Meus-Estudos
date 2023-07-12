215a72a8bf49d07aa3be367b9a7a13107e1b2fb1
IgorSantanaN <124214646+steve-593@users.noreply.github.com>
2023-07-11 22:00:05 -0300

    iteração de arrays

--- telegram/javascript/array/Iteração-em-Arrays.js
+++ telegram/javascript/array/Iteração-em-Arrays.js
@@ -13,8 +13,34 @@ Espera um callback que receba como parâmetro o elemento atual, o índice e o ar
 
 */
 
-let hitchedSpaceships = ['Deméter', 'Darwin', 'Supernove', 'Fenix', 'Puller']
+let hitchedSpaceships = ['Deméter', 'Darwin', 'Supernova', 'Fenix', 'Puller']
 
+//Para cada elemento vai ser realizado a função
 hitchedSpaceships.forEach(function(currentSpaceship, index) {
     console.log('Nave: ' + currentSpaceship + '\nÍndice: ' + index)
 })
+
+console.log('') //para espaçar os exemplos no console
+
+//Realiza uma ação sobre o array sem modificar o array original
+let upcasedSpaceship = hitchedSpaceships.map(function(currentSpaceship) {
+    let upcased = currentSpaceship.toUpperCase()
+    return upcased
+})
+
+console.log(upcasedSpaceship)
+console.log(hitchedSpaceships)
+
+console.log('') //para espaçar os exemplos no console
+
+//Retorna um array com os elementos que passaram no filtro
+let with7Chars = hitchedSpaceships.filter(element => { return element.length >= 7})
+
+console.log(with7Chars)
+
+console.log('') //para espaçar os exemplos no console
+
+//Retorna o primeiro elemento que satisfazer a operação lógica
+let withSevenChars = hitchedSpaceships.find(element => { return element.length >= 7})
+
+console.log(withSevenChars)
