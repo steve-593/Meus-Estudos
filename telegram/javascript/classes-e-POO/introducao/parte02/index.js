e33618b36d7b6fb32550641249c97432f8ccaccd
IgorSantanaN <124214646+steve-593@users.noreply.github.com>
2024-01-23 11:44:12 -0300

    introdução à Classe e POO

---
+++ telegram/javascript/classes-e-POO/introducao/parte02/index.js
@@ -0,0 +1,24 @@
+class Spaceship {
+
+}
+
+let mySpaceship = new Spaceship()
+
+console.log(mySpaceship)
+
+/*
+*O new já foi apresentado quando aprendemos Arrays e Objetos
+*O Array é uma classe Nativa do Javascript
+    *As funções que vimos são métodos internos à classe
+    *forEach, filter, find, findIndex etc.
+    *São criadas no objeto no momento em que chamamos o new
+    *Fazer let myArray = [] é o equivalente a let myArray = new Array()
+*Com Objeto acontece o mesmo quando fazemos myObject = {}
+    *Estamos criando um objeto a partir de umas classe nativa do JS chamada Object
+*Todos os tipo que vimos até agora tem seus tipos associados
+    *String
+    *Number
+    *Boolean
+    *etc.
+*Com as classes, conseguimos criar nossos próprios tipos derivados
+*/
